b0c610d95596b5c58c371b9838d753d2e00d847a
asa <asadbahtiyarov2002@gmail.com>
2022-03-19 23:46:41 +0500

    nothing

--- main.go
+++ main.go
@@ -3,24 +3,7 @@ package main
 import (
 	"html/template"
 	"net/http"
-	"os"
 )
-
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-func (p *Page) save() error {
-	filename := p.Title + "txt"
-	return os.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := title + "txt"
-	body, _ := os.ReadFile(filename)
-	return &Page{Title: title, Body: body}, nil
-}
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
 	tmp, _ := template.ParseFiles("views/index.html")
