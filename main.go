67367d5be4f8e0f0c7cbcdfdc471306efc5db6e7
asa <asadbahtiyarov2002@gmail.com>
2022-03-25 17:36:22 +0500

    all templates is ready

--- main.go
+++ main.go
@@ -1,31 +1,70 @@
 package main
 
 import (
-	"fmt"
+	// "database/sql"
+	// "fmt"
 	"html/template"
+	"log"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-type User struct {
-	Name    string
-	Age     int
-	Balance float64
-	Hobbies []string
+func index(w http.ResponseWriter, r *http.Request) {
+	tm, err := template.ParseFiles("template/index.html", "template/header.html", "template/footer.html")
+	if err != nil {
+		panic(err)
+	}
+	err = tm.ExecuteTemplate(w,"index", nil)
+	if err != nil {
+		panic(err)
+	}
 }
+func post(w http.ResponseWriter, r *http.Request) {
+	tm, err := template.ParseFiles("template/post.html", "template/header.html", "template/footer.html")
+	if err != nil {
+		panic(err)
+	}
+	err = tm.ExecuteTemplate(w,"post", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+func about(w http.ResponseWriter, r *http.Request) {
+	tm, err := template.ParseFiles("template/about.html", "template/header.html", "template/footer.html")
+	if err != nil {
+		panic(err)
+	}
+	err = tm.ExecuteTemplate(w,"about", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+func contact(w http.ResponseWriter, r *http.Request) {
+	tm, err := template.ParseFiles("template/contact.html", "template/header.html", "template/footer.html")
+	if err != nil {
+		panic(err)
+	}
+	err = tm.ExecuteTemplate(w,"contact", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+func handleFunc() {
+	// Static files
+	fs := http.FileServer(http.Dir("./static/")) 
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-func mainPage(w http.ResponseWriter, r *http.Request) {
-	tmp, err := template.ParseFiles("views/index.html")
-	if err != nil {
-		fmt.Printf("Error on parcing tamplate: error %v", err)
-	}
-	data := User{"Nazarbek", 13, 150.12, []string{"Coding", "Music", "BeatMaking"}}
-	err = tmp.Execute(w, data)
-	if err != nil {
-		fmt.Printf("Error on executing template: error %v", err)
-	}
+	// Handling requests
+	http.HandleFunc("/contact", contact)
+	http.HandleFunc("/about", about)
+	http.HandleFunc("/post", post)
+	http.HandleFunc("/", index)
+
+
+	log.Fatalln(http.ListenAndServe(":8080", nil))
 }
 
 func main() {
-	http.HandleFunc("/", mainPage)
-	http.ListenAndServe(":8080", nil)
+	handleFunc()
 }
